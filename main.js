34fea8bb2e9f348b261959eb99e07e0184a0e33d
halishaah <111658343+halishaah@users.noreply.github.com>
2022-11-20 21:26:28 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+
+const number_URL = 'http://numbersapi.com/';
+let enter_btn = document.querySelector(".btn");
+let card = document.querySelector(".card");
+
+
+enter_btn.addEventListener("click", getFacts);
+enter_btn.addEventListener("click", getMotion);
+
+function getFacts() {
+  let input = document.querySelector(".input").value;
+  let card_title = document.querySelector(".card-title");
+  let card_text = document.querySelector(".card-text");
+  const promise = fetch(number_URL +input)
+  promise
+  .then((response)=>response.text())
+  .then((data)=>{
+    card_title.innerHTML = input;
+    card_text.innerHTML = data;
+  });
+}
+
+const container = popmotion.styler(document.querySelector('.wrapper'));
+        
+   
+        function getMotion() {
+        popmotion.tween({
+            from: {
+                scale: .7,
+                opacity: 0,
+                y:0
+            },
+            to: {
+                scale:1,
+                opacity:1,
+                y:200
+            },
+            duration:1000
+        }).start(container.set)
+    }
+
+
